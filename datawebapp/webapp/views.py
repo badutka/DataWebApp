5deaa5d01be3fdee7ba69b5e454b8cfe26f5ca3d
Bartosz Dutka <dutkabar@gmail.com>
2022-11-14 14:23:14 +0100

    added basic html files and views

--- datawebapp/webapp/views.py
+++ datawebapp/webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'webapp/home.html', {'nbar': 'home'})
+
+
+def about(request):
+    context = {'title': 'about', 'nbar': 'about'}
+    # data view, edit and automation tool DVEAT
+    return render(request, 'webapp/about.html', context=context)
