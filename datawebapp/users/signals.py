e3db02ebd51c39f356414b6ca7a68f5fbaff94ec
Bartosz Dutka <dutkabar@gmail.com>
2022-11-16 19:08:17 +0100

    added status change on login/logout to profile

--- datawebapp/users/signals.py
+++ datawebapp/users/signals.py
@@ -1,4 +1,5 @@
 from django.db.models.signals import post_save
+from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
@@ -13,3 +14,16 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+@receiver(user_logged_in)
+def update_status_on_login(sender, user, **kwargs):
+    user.profile.status = True
+    user.profile.save()
+
+
+@receiver(user_logged_out)
+def update_status_on_logout(sender, user, **kwargs):
+    if user:
+        user.profile.status = False
+        user.profile.save()
