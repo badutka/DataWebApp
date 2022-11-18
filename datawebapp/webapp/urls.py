113d0d2fc6d1a13f3d8a3b6d408c97a44d66a91e
Bartosz Dutka <dutkabar@gmail.com>
2022-11-18 14:48:33 +0100

    added actions/data_summary sub-tab and actions tab

--- datawebapp/webapp/urls.py
+++ datawebapp/webapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='webapp-home'),
     path('about/', views.about, name='webapp-about'),
+    path('data_summary/', views.data_summary, name='webapp-data_summary'),
 ]
