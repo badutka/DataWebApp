eaa59cea13649175e874b4346985096ff4a4d441
Bartosz Dutka <dutkabar@gmail.com>
2022-11-29 00:28:51 +0100

    added js script to turn on bs5 tooltips

--- datawebapp/webapp/static/webapp/main.js
+++ datawebapp/webapp/static/webapp/main.js
@@ -1,5 +1,12 @@
 $(".summary-data-table-toggler").hide();
 
+// initialize tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+//
+
 $(document).ready(function () {
 
 $('#sidebarCollapse').on('click', function () {
