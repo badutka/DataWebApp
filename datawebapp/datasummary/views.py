a75013ff886e03c1f9409c5c757457bc40c4fa1a
Bartosz Dutka <dutkabar@gmail.com>
2022-11-30 22:45:43 +0100

    added more df details in table footer

--- datawebapp/datasummary/views.py
+++ datawebapp/datasummary/views.py
@@ -23,8 +23,8 @@ def data_summary(request):
             (col_count, dtypes, non_null_counts, dtype_counts, memory_usage_string) = get_df_details(df)
             # noinspection PyTypeChecker,PyUnresolvedReferences
             # df_details = df_details(col_count, zip(dtypes.index, dtypes.values, non_null_counts.values), zip(non_null_counts.index, non_null_counts.values), dtype_counts, memory_usage_string)
-            # df_details = {'index': dtypes.index, 'dtypes': dtypes.values, 'non_nulls': non_null_counts.values}
-            df_details = zip(dtypes.index, dtypes.values, non_null_counts.values)
+            df_details = {'index': dtypes.index, 'dtypes': dtypes.values, 'non_nulls': non_null_counts.values}
+            # df_details = zip(dtypes.index, dtypes.values, non_null_counts.values)
             # noinspection PyTypeChecker,PyUnresolvedReferences
             column_details = {'col_count': col_count, 'dtype_counts': zip(dtype_counts.index, dtype_counts.values), 'memuse':  memory_usage_string}
             context.update({'df': df, 'df_head': df_head, 'df_details': df_details, 'column_details': column_details})
